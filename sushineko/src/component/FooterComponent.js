e66936e22a1159694473086ee6174a86d4ea7309
Maiko <maikokuroki24@gmail.com>
2020-07-12 21:51:50 -0600

    a

---
+++ sushineko/src/component/FooterComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = (props) =>{
+    return(
+        <footer className="site-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        Warning:All the food prepared by master chef Neko<i className="fa fa-utensils" />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
