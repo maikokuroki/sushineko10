6b92f4fc643e2765d023c3d0edbe610ed706b007
amykeonucamp <amy@nucamp.co>
2020-07-19 19:14:36 -0700

    Amy. Created new component to have add to cart functions.
    Maiko, you need to do that same to the other menu items. In Week 5, there are exercises to save data to a file. Once you save data to a file, you can create an Order component to read from that file.

--- sushineko/src/component/MenuComponent.js
+++ sushineko/src/component/MenuComponent.js
@@ -9,84 +9,57 @@ import {
 	CardTitle,
 	Button,
 	Label,
-	Table
+	Table,
 } from "reactstrap";
 
-import { NIGIRIMENU } from "../shared/Nigiri";
+import Nigiri from "./NigiriMenuComponent";
+
 import { ROLLSMENU } from "../shared/Rolls";
 import { SIDEMENU } from "../shared/Sidemenu";
 import { SPECIALSMENU } from "../shared/Specials";
 import { render } from "@testing-library/react";
-import CustomerInfo from './CustomerInfo';
-
-function Nigiri({ nigiriMenu }) {
-	return (
-		<React.Fragment>
-			<h1>= Nigiri =</h1>
-		<CardDeck>
-			{nigiriMenu.map((nigiri) => {
-				return (
-					<Card>
-						<CardImg top src={nigiri.image} alt={nigiri.name} width='50' height='150' />
-						<CardTitle className='CardTitle fluid'>{nigiri.name}</CardTitle>
-						<CardBody>
-							<CardText>
-								{nigiri.description} <br />
-								Price:${nigiri.price}
-								<hr />
-								<Label htmlFor={nigiri.name}>Quantity</Label>
-                    				<select class="form-control" id={nigiri.name}>
-                      					<option value="0" selected>0</option>
-                      					<option value="1">1</option>
-                      					<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-									</select>
-								<br />
-								<Button type='submit'>Add to the Cart</Button>
-							</CardText>
-						</CardBody>
-					</Card>
-				);
-			})}
-		</CardDeck>
-		</React.Fragment>
-	);
-}
+import CustomerInfo from "./CustomerInfo";
 
 function Rolls({ rollsMenu }) {
 	return (
 		<React.Fragment>
 			<h1>= Rolls =</h1>
-		<CardDeck>
-			{rollsMenu.map((roll) => {
-				return (
-					<Card>
-						<CardImg top src={roll.image} alt={roll.name} width='50' height='150' />
-						<CardTitle className='CardTitle fluid'>{roll.name}</CardTitle>
-						<CardBody>
-							<CardText>
-								{roll.description} <br />
-								Price:${roll.price}
-								<hr />
-								<Label htmlFor={roll.name}>Quantity</Label>
-                    				<select class="form-control" id={roll.name}>
-                      					<option value="0" selected>0</option>
-                      					<option value="1">1</option>
-                      					<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
+			<CardDeck>
+				{rollsMenu.map((roll) => {
+					return (
+						<Card>
+							<CardImg
+								top
+								src={roll.image}
+								alt={roll.name}
+								width='50'
+								height='150'
+							/>
+							<CardTitle className='CardTitle fluid'>{roll.name}</CardTitle>
+							<CardBody>
+								<CardText>
+									{roll.description} <br />
+									Price:${roll.price}
+									<hr />
+									<Label htmlFor={roll.name}>Quantity</Label>
+									<select class='form-control' id={roll.name}>
+										<option value='0' selected>
+											0
+										</option>
+										<option value='1'>1</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
 									</select>
-								<br />
-								<Button type='submit'>Add to the Cart</Button>
-							</CardText>
-						</CardBody>
-					</Card>
-				);
-			})}
-		</CardDeck>
+									<br />
+									<Button type='submit'>Add to the Cart</Button>
+								</CardText>
+							</CardBody>
+						</Card>
+					);
+				})}
+			</CardDeck>
 		</React.Fragment>
 	);
 }
@@ -95,34 +68,42 @@ function Specials({ specialsMenu }) {
 	return (
 		<React.Fragment>
 			<h1>= Specials =</h1>
-		<CardDeck>
-			{specialsMenu.map((specials) => {
-				return (
-					<Card>
-						<CardImg top src={specials.image} alt={specials.name} width='50' height='300' />
-						<CardTitle className='CardTitle fluid'>{specials.name}</CardTitle>
-						<CardBody>
-							<CardText>
-								{specials.description} <br />
-								Price:${specials.price}
-								<hr />
-								<Label htmlFor={specials.name}>Quantity</Label>
-                    				<select class="form-control" id={specials.name}>
-                      					<option value="0" selected>0</option>
-                      					<option value="1">1</option>
-                      					<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
+			<CardDeck>
+				{specialsMenu.map((specials) => {
+					return (
+						<Card>
+							<CardImg
+								top
+								src={specials.image}
+								alt={specials.name}
+								width='50'
+								height='300'
+							/>
+							<CardTitle className='CardTitle fluid'>{specials.name}</CardTitle>
+							<CardBody>
+								<CardText>
+									{specials.description} <br />
+									Price:${specials.price}
+									<hr />
+									<Label htmlFor={specials.name}>Quantity</Label>
+									<select class='form-control' id={specials.name}>
+										<option value='0' selected>
+											0
+										</option>
+										<option value='1'>1</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
 									</select>
-								<br />
-								<Button type='submit'>Add to the Cart</Button>
-							</CardText>
-						</CardBody>
-					</Card>
-				);
-			})}
-		</CardDeck>
+									<br />
+									<Button type='submit'>Add to the Cart</Button>
+								</CardText>
+							</CardBody>
+						</Card>
+					);
+				})}
+			</CardDeck>
 		</React.Fragment>
 	);
 }
@@ -131,54 +112,57 @@ function Sidemenu({ sideMenu }) {
 	return (
 		<React.Fragment>
 			<h1>= Side Menu =</h1>
-		<CardDeck>
-			{sideMenu.map((sidemenu) => {
-				return (
-					<Card>
-						<CardImg top src={sidemenu.image} alt={sidemenu.name} width='50' height='150' />
-						<CardTitle className='CardTitle'>{sidemenu.name}</CardTitle>
-						<CardBody>
-							<CardText>
-								{sidemenu.description} <br />
-								Price:${sidemenu.price}<br />
-								<hr />
-								<Label htmlFor={sidemenu.name}>Quantity</Label>
-                    				<select class="form-control" id={sidemenu.name}>
-                      					<option value="0" selected>0</option>
-                      					<option value="1">1</option>
-                      					<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
+			<CardDeck>
+				{sideMenu.map((sidemenu) => {
+					return (
+						<Card>
+							<CardImg
+								top
+								src={sidemenu.image}
+								alt={sidemenu.name}
+								width='50'
+								height='150'
+							/>
+							<CardTitle className='CardTitle'>{sidemenu.name}</CardTitle>
+							<CardBody>
+								<CardText>
+									{sidemenu.description} <br />
+									Price:${sidemenu.price}
+									<br />
+									<hr />
+									<Label htmlFor={sidemenu.name}>Quantity</Label>
+									<select class='form-control' id={sidemenu.name}>
+										<option value='0' selected>
+											0
+										</option>
+										<option value='1'>1</option>
+										<option value='2'>2</option>
+										<option value='3'>3</option>
+										<option value='4'>4</option>
+										<option value='5'>5</option>
 									</select>
-								
-								<br />
-								<Button type='submit'>Add to the Cart</Button>
-							</CardText>
-						</CardBody>
-					</Card>
-				);
-			})}
-		</CardDeck>
+									<br />
+									<Button type='submit'>Add to the Cart</Button>
+								</CardText>
+							</CardBody>
+						</Card>
+					);
+				})}
+			</CardDeck>
 		</React.Fragment>
 	);
 }
-
-
 
 class Menu extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			nigiriMenu: NIGIRIMENU,
 			rollsMenu: ROLLSMENU,
 			sideMenu: SIDEMENU,
 			specialsMenu: SPECIALSMENU,
 		};
 	}
-
-	
 
 	render() {
 		return (
@@ -190,7 +174,7 @@ class Menu extends Component {
 					<Sidemenu sideMenu={this.state.sideMenu} />
 
 					<h2>Order Details</h2>
-					
+
 					<Table>
 						<thead>
 							<tr>
@@ -202,10 +186,10 @@ class Menu extends Component {
 						</thead>
 						<tbody>
 							<tr>
-								<td id="item"></td>
-								<td id="quantity"></td>
-								<td id="price"></td>
-								<td id="total"></td>
+								<td id='item'></td>
+								<td id='quantity'></td>
+								<td id='price'></td>
+								<td id='total'></td>
 							</tr>
 						</tbody>
 						<tfoot>
@@ -213,7 +197,7 @@ class Menu extends Component {
 								<td>
 									<h5>Subtotal</h5>
 								</td>
-								<td id="orderSubtotal"></td>
+								<td id='orderSubtotal'></td>
 							</tr>
 						</tfoot>
 					</Table>
---
+++ sushineko/src/component/NigiriMenuComponent.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import {
+	CardDeck,
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	Button,
+	Label,
+	Form,
+} from "reactstrap";
+
+import { NIGIRIMENU } from "../shared/Nigiri";
+
+class Nigiri extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			nigiriMenu: NIGIRIMENU,
+			//quantity: 0,
+
+			order: [],
+		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target;
+		const quantity = target.value;
+		const itemName = target.name;
+
+		this.setState({
+			order: [...this.state.order, { quantity, itemName }],
+		});
+	}
+
+	handleSubmit(event) {
+		console.log("Order: " + JSON.stringify(this.state.order));
+		alert("Order: " + JSON.stringify(this.state.order));
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1>= Nigiri =</h1>
+				<CardDeck>
+					{this.state.nigiriMenu.map((nigiri) => {
+						return (
+							<Card>
+								<CardImg
+									top
+									src={nigiri.image}
+									alt={nigiri.name}
+									width='50'
+									height='150'
+								/>
+								<CardTitle className='CardTitle fluid'>{nigiri.name}</CardTitle>
+								<CardBody>
+									<CardText>
+										<Form onSubmit={this.handleSubmit}>
+											{nigiri.description} <br />
+											Price:${nigiri.price}
+											<hr />
+											<Label htmlFor={nigiri.name}>Quantity</Label>
+											<select
+												class='form-control'
+												id={nigiri.name}
+												name={nigiri.name}
+												value={this.state.quantity}
+												onChange={this.handleInputChange}>
+												<option value='0' selected>
+													0
+												</option>
+												<option value='1'>1</option>
+												<option value='2'>2</option>
+												<option value='3'>3</option>
+												<option value='4'>4</option>
+												<option value='5'>5</option>
+											</select>
+											<br />
+											<Button type='submit'>Add to the Cart</Button>
+										</Form>
+									</CardText>
+								</CardBody>
+							</Card>
+						);
+					})}
+				</CardDeck>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Nigiri;
